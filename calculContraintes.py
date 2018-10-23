ddc448014d80623f9062f715b5935b0848f5d19a
FrancoisRem <44405815+francoisrem@users.noreply.github.com>
2018-10-23 21:07:36 +0200

    Changement de Référentiel OK

--- calculContraintes.py
+++ calculContraintes.py
@@ -1,1 +1,22 @@
 import numpy as np
+
+
+n = 1.0
+
+def basisChange(X,Y,Z,G):
+    L = np.sqrt(G[0]**2 + G[1]**2 + G[2]**2)
+    l = np.sqrt(G[0] ** 2 + G[1] ** 2)
+    '''R = np.array([[-G[0]/L,-G[1]/L,-G[2]/L],
+                  [-G[1]/l,G[0]/l,0],
+                  [-(0-G[2])*G[0]/l/L,-(0-G[2])*G[1]/l/L,-(G[0]**2+G[1]**2)/l/L]])'''
+    R = np.array([[-G[0] / L, -G[1] / L, -G[2] / L],
+                  [G[1] / l, -G[0] / l, 0],
+                  [ -G[2] * G[0] / l / L, -G[2] * G[1] / l / L, (G[0] ** 2 + G[1] ** 2) / l / L]])
+    print(R)
+    #R = np.linalg.inv(R)
+    M = np.array([[X,Y,Z]])
+    G = np.array([[G[0],G[1],G[2]]])
+
+    return R.dot(M.T - G.T)
+
+print(basisChange(0,0,0,[-1,1,1]))
