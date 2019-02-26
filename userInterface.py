0c16ca0653de81a8a72e0a8c62b26f14989719e1
Ines Harang <ines.harang@mines-paristech.fr>
2019-02-26 17:56:57 +0100

    first modif userInterface

--- userInterface.py
+++ userInterface.py
@@ -1,1 +1,7 @@
 '''This python Script allows the user to choose its input parameters and get the sun trajectory for the experience'''
+
+## Trajectoire
+
+def soleilSphereCeleste (thetaS, phiS, thetaP, phiP):
+    
+# info à connaître en entrée : la latitude et la longitude souhaitée ; l'exposition de la maisonette (vitre orientée vers quel pt cardinal?) 
